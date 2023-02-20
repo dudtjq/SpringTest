43165e2be35db041c1bb12c76c952a9ad6ecdc43
DESKTOP-KM2AE57\user <dudtjq>
2023-02-20 18:28:08 +0900

    [이영섭] 1. Spring Test 프로젝트 파일 추가

---
+++ src/test/java/com/dudtjq/test/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dudtjq.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
